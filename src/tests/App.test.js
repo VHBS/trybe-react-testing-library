a8fb1c1629c1912755714c10abdfafa73ecee879
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-11-30 14:27:50 -0300

    Requisito - 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 1 - Testa o componente App.js', () => {
+  it('Testa se o primeiro link possui o texto Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const buttonHome = screen.getByRole('link', {
+      name: /home/i });
+
+    expect(buttonHome).toBeInTheDocument();
+    userEvent.click(buttonHome);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Testa se o primeiro link possui o texto About', () => {
+    const { history } = renderWithRouter(<App />);
+    const buttonAbout = screen.getByRole('link', {
+      name: /about/i });
+    expect(buttonAbout).toBeInTheDocument();
+
+    userEvent.click(buttonAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Testa se o primeiro link possui o texto Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    const buttonFavoritePokémons = screen.getByRole('link', {
+      name: /favorite pokémons/i });
+    expect(buttonFavoritePokémons).toBeInTheDocument();
+
+    userEvent.click(buttonFavoritePokémons);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Testa se o primeiro link possui o texto NotFound', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/blablabla');
+
+    const text = screen.getByRole('heading', {
+      name: /page requested not found/i });
+    expect(text).toBeInTheDocument();
+  });
+});
