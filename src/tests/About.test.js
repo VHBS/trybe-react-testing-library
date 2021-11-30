433b80fe99cc69948d9fa33be843e65ad4f4385f
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-11-30 16:00:21 -0300

    Requisito - 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { About } from '../components';
+
+describe('Requisito 2 - Testa o componente About', () => {
+  it('Teste de a página contém as informações dobre a Pokédex', () => {
+    render(<About />);
+    const title = screen.getByRole('heading', {
+      name: /about pokédex/i });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com o texto sobre Pokédex', () => {
+    render(<About />);
+    const pokedexTextOne = screen.getByText(/this application simulates a pokédex/i);
+    const pokedexTextTwo = screen.getByText(/one can filter pokémons by type/i);
+    expect(pokedexTextOne && pokedexTextTwo).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    render(<About />);
+    const img = screen.getByRole('img', {
+      name: /pokédex/i });
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
